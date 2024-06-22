a005904f694290af3bce20ac1c61892d4cff733b
Hassam <hassamkhan98@outlook.com>
2024-06-22 18:00:21 +0200

    Lazy and Eager concept

---
+++ EagerandLazy/src/main/java/com/Hibernate/ToMany/Questions.java
@@ -0,0 +1,67 @@
+package com.Hibernate.ToMany;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Questions {
+
+	@Id
+	private int question_id;
+	private String question;
+	
+	
+//	fetch work on relations mapping woh data jo dosray table sy araha ho usko lazy ya eager krna
+//	                                   by default eager hota hai
+	@OneToMany( mappedBy="questions", fetch = FetchType.LAZY)
+	private List<Answers> answers;
+	
+	
+	
+	
+	public int getQuestion_id() {
+		return question_id;
+	}
+	public void setQuestion_id(int question_id) {
+		this.question_id = question_id;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public List<Answers> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(List<Answers> answers) {
+		this.answers = answers;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(answers, question, question_id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Questions other = (Questions) obj;
+		return Objects.equals(answers, other.answers) && Objects.equals(question, other.question)
+				&& question_id == other.question_id;
+	}
+	
+	
+	
+	
+	
+	
+}
